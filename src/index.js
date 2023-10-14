b06923efef122964628da7217364d8218bb79644
gen <regan035@gmail.com>
2023-10-14 18:19:06 -0500

    add robots

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <div>
     <Card />
-  </React.StrictMode>
+    <Card />
+    <Card />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
