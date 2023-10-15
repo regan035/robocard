7ff7351c9a8ee3104766a415fb2fbc24b5e4dad0
gen <regan035@gmail.com>
2023-10-15 13:43:38 -0500

    update searchfield states and pass search result to cardlist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 // using {}destructure for non-export default
 import { robots } from "./robots";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />;
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    //set state for search field
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    //filter robots matches with input field
+    const filterRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />;
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
+        // every time type is changed we call this function
+        onChange={searchChange}
       />
     </div>
   );
